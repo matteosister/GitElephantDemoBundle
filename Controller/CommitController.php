0e6c0b19e833563465f7de6d3acca04a361b3981
matteosister <matteog@gmail.com>
2012-01-06 23:24:43 +0100

    initial import

---
+++ Controller/CommitController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: matteo
+ * Date: 14/11/11
+ * Time: 22.56
+ *
+ * Just for fun...
+ */
+
+namespace Cypress\GitElephantDemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class CommitController extends Controller
+{
+    /**
+     * @Route("/commit/{ref}", name="repository_commit")
+     * @Template()
+     *
+     * @param $ref The treeish reference
+     */
+    public function showAction($ref)
+    {
+        $commit = $this->get('git_repository')->getCommit($ref);
+        $diff = $this->get('git_repository')->getDiff($commit);
+        return array(
+            'repository'    => $this->get('git_repository'),
+            'commit'        => $commit,
+            'diff'          => $diff
+        );
+    }
+}
---
+++ CypressGitElephantDemoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Cypress\GitElephantDemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CypressGitElephantDemoBundle extends Bundle
+{
+}
